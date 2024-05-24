bacb9ff1519c4468f1806ebfe5e7980db5bf2a3a
AlexandrKosov123 <159529311+AlexandrKosov123@users.noreply.github.com>
2024-05-24 13:45:55 +0300

    CW1_Kosov_reattempt

---
+++ 1CW_1t_4var.cs
@@ -0,0 +1,115 @@
+﻿// КР1. Вариант 4. Задача 2.
+
+using System;
+using System.Linq;
+
+abstract class Shape
+{
+    public abstract double CalculateArea();
+    public abstract double CalculatePerimeter();
+}
+
+class Round : Shape
+{
+    private double radius;
+
+    public Round(double radius)
+    {
+        this.radius = radius;
+    }
+
+    public override double CalculateArea()
+    {
+        return Math.PI * radius * radius;
+    }
+
+    public override double CalculatePerimeter()
+    {
+        return 2 * Math.PI * radius;
+    }
+}
+
+class Square : Shape
+{
+    private double side;
+
+    public Square(double side)
+    {
+        this.side = side;
+    }
+
+    public override double CalculateArea()
+    {
+        return side * side;
+    }
+
+    public override double CalculatePerimeter()
+    {
+        return 4 * side;
+    }
+}
+
+class Triangle : Shape
+{
+    private double a, b, c;
+
+    public Triangle(double a, double b, double c)
+    {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public override double CalculateArea()
+    {
+        double p = (a + b + c) / 2;
+        return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+    }
+
+    public override double CalculatePerimeter()
+    {
+        return a + b + c;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Shape[] shapes = new Shape[]
+        {
+            new Round(5),
+            new Round(3),
+            new Round(7),
+            new Round(2),
+            new Round(4),
+            new Square(4),
+            new Square(3),
+            new Square(5),
+            new Square(2),
+            new Square(6),
+            new Triangle(3, 4, 5),
+            new Triangle(6, 8, 10),
+            new Triangle(5, 12, 13),
+            new Triangle(8, 15, 17),
+            new Triangle(7, 24, 25)
+        };
+
+        Console.WriteLine("Таблица: Название фигуры, Периметр, Площадь\n");
+
+        var sortedByArea = shapes.OrderBy(s => s.CalculateArea()).ToList();
+
+        PrintTable(sortedByArea);
+
+        Console.ReadLine();
+    }
+
+    static void PrintTable(List<Shape> shapes)
+    {
+        Console.WriteLine("{0,-10} {1,-10} {2,-10}", "Фигура", "Периметр", "Площадь");
+        foreach (var shape in shapes)
+        {
+            Console.WriteLine("{0,-10} {1,-10:F2} {2,-10:F2}", shape.GetType().Name, shape.CalculatePerimeter(), shape.CalculateArea());
+        }
+    }
+}
---
+++ 1CW_2t_4var.cs
@@ -0,0 +1,72 @@
+﻿/* Вариант 4.
+   Задача 1.
+*/
+
+using System;
+using System.Linq;
+
+struct Triangle
+{
+    public double[] sides;
+
+    public Triangle(double[] sides)
+    {
+        if (sides.Length != 3)
+            throw new ArgumentException("Triangle must have exactly 3 sides");
+        this.sides = sides;
+    }
+
+    public string Type()
+    {
+        if (sides[0] == sides[1] && sides[1] == sides[2])
+            return "Равносторонний";
+        else if (sides[0] == sides[1] || sides[1] == sides[2] || sides[0] == sides[2])
+            return "Равнобедренный";
+        else
+            return "Разносторонний";
+    }
+
+    public double Area()
+    {
+        double p = sides.Sum() / 2.0;
+        return Math.Sqrt(p * (p - sides[0]) * (p - sides[1]) * (p - sides[2]));
+    }
+
+    public void PrintInfo()
+    {
+        Console.WriteLine($"Длины сторон: {sides[0]}, {sides[1]}, {sides[2]}");
+        Console.WriteLine($"Площадь: {Area()}");
+        Console.WriteLine($"Тип: {Type()}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Triangle[] triangles = new Triangle[5];
+
+        // Заполнение массива треугольников
+        triangles[0] = new Triangle(new double[] { 3, 4, 5 });
+        triangles[1] = new Triangle(new double[] { 5, 5, 5 });
+        triangles[2] = new Triangle(new double[] { 4, 4, 3 });
+        triangles[3] = new Triangle(new double[] { 6, 8, 10 });
+        triangles[4] = new Triangle(new double[] { 7, 24, 25 });
+
+        // Сортировка массива по уменьшению площади
+        Array.Sort(triangles, (x, y) => x.Area().CompareTo(y.Area()));
+
+        // Вывод информации о треугольниках в виде таблицы
+        Console.WriteLine("Треугольники, отсортированные по уменьшению площади:");
+        Console.WriteLine("--------------------------------------------------");
+        Console.WriteLine("|   Длины сторон   |   Площадь   |   Тип   |");
+        Console.WriteLine("--------------------------------------------------");
+        foreach (var triangle in triangles)
+        {
+            Console.Write($"|   {triangle.sides[0]}, {triangle.sides[1]}, {triangle.sides[2]}   ");
+            Console.Write($"|   {triangle.Area()}   ");
+            Console.Write($"|   {triangle.Type()}   |\n");
+        }
+        Console.WriteLine("--------------------------------------------------");
+    }
+}
